f705745662ef9d5f3a44d0f190ddf7da36b0f941
borysiuk-getplume <oborysiuk@getplume.co>
2025-05-21 23:10:20 +0300

    featured in corrected and swiper added

---
+++ src/components/icons/ArrowNextIcon.tsx
@@ -0,0 +1,12 @@
+const ArrowNextIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12.398 25.002a1.5 1.5 0 0 1 0-2.122L17.996 17l-5.598-5.598a1.5 1.5 0 1 1 2.122-2.121l6.8 6.8a1.5 1.5 0 0 1 0 2.121l-6.8 6.8a1.5 1.5 0 0 1-2.122 0Z"
+      fill="#6816AF"
+    />
+  </svg>
+);
+
+export default ArrowNextIcon;
