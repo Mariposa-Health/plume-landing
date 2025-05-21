8719a382f44d0f29b46eb536cd714aea3c2d1fc1
borysiuk-getplume <oborysiuk@getplume.co>
2025-05-21 20:47:13 +0300

    header corrected

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,26 +1,47 @@
+'use client'
+
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
 import Button from './Button';
 import Wrapper from './Wrapper';
 
 export default function Header() {
-  return (
-    <header className="w-full py-3 lg:py-12">
-      <Wrapper>
-        <div className="flex items-center justify-between">
-          <a href="https://getplume.co/" className="w-[106px] lg:w-[153px] h-[36px] lg:h-[52px]">
-            <Image
-              src="/logo.svg"
-              alt="Plume logo"
-              width={153}
-              height={52}
-              className="w-full h-full"
-            />
-          </a>
+  const [isScrolled, setIsScrolled] = useState(false);
 
-          <Button variant="secondary" href="https://getplume.co/get-started">
-            Get Started
-          </Button>
-        </div>
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <header
+      className={`sticky top-0 w-full py-[20px] lg:py-[24px] bg-white z-100 transition-all duration-200 ${
+        isScrolled ? 'border-b border-[#6816AF]/25' : ''
+      }`}
+    >
+      <Wrapper className="flex items-center justify-between gap-[16px]">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="Plume logo"
+            width={153}
+            height={52}
+            className="h-[40px] lg:h-[52px] w-auto"
+          />
+        </Link>
+
+        <Button
+          className="whitespace-nowrap"
+          variant="secondary"
+          href="https://getplume.co/get-started"
+        >
+          Get Started
+        </Button>
       </Wrapper>
     </header>
   );
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
   const isMobile = useMedia('lg');
 
   return (
-    <Section className="flex flex-col lg:justify-center gap-[32px] md:gap-[40px] lg:min-h-[calc(100vh-148px)] !pt-[0px] lg:!pt-[100px] lg:bg-[#3A0469] overflow-hidden">
+    <Section className="flex flex-col lg:justify-center gap-[32px] md:gap-[40px] lg:min-h-[calc(100vh-100px)] !pt-[0px] lg:!pt-[100px] lg:bg-[#3A0469] overflow-hidden">
       <picture className="w-full lg:h-full lg:absolute lg:inset-0 z-0">
         <source media="(min-width: 1024px)" srcSet="/images/hero/hero-image-desktop.jpg" />
         <Image
