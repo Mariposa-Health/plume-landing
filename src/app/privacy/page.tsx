21570b17e08f328c4ef0b585e399bd1a5b497625
borysiuk-getplume <oborysiuk@getplume.co>
2025-05-22 21:07:09 +0300

    force-static added to privacy page

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import Section from '@/components/common/Section';
 import Wrapper from '@/components/common/Wrapper';
 
+export const dynamic = 'force-static';
+
 export const metadata: Metadata = {
   title: 'Privacy Policy - Plume Health',
   description:
